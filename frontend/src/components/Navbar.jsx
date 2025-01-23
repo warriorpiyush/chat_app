3ce541a82c48b7bdffa8544277b21f6bf7a533a0
Piyush_Gupta <115005783+warriorpiyush@users.noreply.github.com>
2025-01-23 23:06:44 +0530

    project done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
@@ -17,7 +18,7 @@ const Navbar = () => {
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <h1 className="text-lg font-bold">Tap Talk</h1>
             </Link>
           </div>
 
